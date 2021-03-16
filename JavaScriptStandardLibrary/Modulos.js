20bf9ab46393a3bfc4ed55cd7bdc4e737486f0c2
Alexis Sierra <fasierra@personalsoft.com.co>
2021-03-16 15:34:31 -0500

    Modulos y ejercicio

---
+++ JavaScriptStandardLibrary/Modulos.js
@@ -0,0 +1,93 @@
+/*
+* Modulos. Permite que grandes programas se ensamblen, y que funcionen correctaente
+* consiste en ocultar detalles de implementación privada,
+* mantener ordenado al namespace global para que los modulos no puedan modificar accidentalmente las
+* variables, funciones y clases definidas por otros modulos.
+*/
+
+const stats = (function() {
+	const sum = (x,y) => x + y;
+	const square = x => x * x;
+	
+	function mean(data) {
+		return data.reduce(sum)/data.length;
+	}
+	
+	function stdDev(data) {
+		let m = mean(data);
+		return Math.sqrt(
+			data.map(x => x - m).map(square).reduce(sum)/(data.length - 1)
+		);
+	}
+	
+	return {
+		mean,
+		//sum,
+		stdDev
+	};
+}());
+
+console.log(stats.mean([1, 3, 5, 7, 9]));
+console.log(stats.stdDev([1, 3, 5, 7, 9]));
+
+//console.log(stats.sum(1, 3));
+
+const calculadora = (function() {
+	const multiplicar = (x, y) => x * y;
+	const restar = (x, y) => x - y;
+	const dividir = (x, y) => x / y;
+	const sumar = (x, y) => x + y;
+	
+	return {
+		multiplicar,
+		restar,
+		dividir,
+		sumar
+	}
+}());
+
+console.log(calculadora.multiplicar(7,3));
+
+const modules = {};
+
+function require(moduleName) {
+	return modules[moduleName];
+}
+
+modules['stats.js'] = (function() {
+	const exports = {};
+	
+	const sum = (x,y) => x + y;
+	const square = x => x * x;
+	
+	exports.mean = function (data) {
+		return data.reduce(sum)/data.length;
+	}
+	
+	exports.stdDev = function (data) {
+		let m = mean(data);
+		return Math.sqrt(
+			data.map(x => x - m).map(square).reduce(sum)/(data.length - 1)
+		);
+	}
+	
+	return exports;
+}());
+
+modules['calculadora.js'] = (function() {
+	const exports = {};
+	
+	exports.multiplicar = function (x, y){ return x * y };
+	exports.restar = function (x, y) { return x - y };
+	exports.dividir = function (x, y) { return x / y };
+	exports.sumar = function (x, y) { return x + y };
+	
+	return exports;
+}());
+
+const stats2 = require('stats.js');
+const calculadora2 = require('multiplicar.js');
+
+console.log(stats2.mean([1, 3, 5, 7, 9]));
+console.log(stats2.stdDev([1, 3, 5, 7, 9]));
+console.log(calculadora2.dividir(7,3));
---
+++ JavaScriptStandardLibrary/ServiceCenter.js
@@ -0,0 +1,88 @@
+
+/*
+* Ejercicio Modulos
+* Centro de Servicios, recargas, envios, pagos...
+*/
+class NumberFormatError extends Error {
+	
+	constructor (operacion, statusText, numero) {
+		super(`${operacion} ${statusText}: ${numero}`);	
+
+		this.operacion = operacion;
+		this.statusText = statusText;
+		this.numero = numero;
+	}
+	
+	get name () {
+		return "NumberFormatError"; 
+	}	
+	
+}
+
+function validarNumero(numero) {
+	let condicion = /[^0-9]+/;
+	if ((numero+"").match(condicion) == null) {
+		throw new NumberFormatError("Error Numero", "El numero no es valido.", numero);
+	}
+}
+
+const modules = {};
+
+function require(moduleName) {
+	return modules[moduleName];
+}
+
+modules['recargas.js'] = (function (){
+	const exports = {};
+	
+	let registroRecargas = new Map(); 
+	
+	const operadores = ['CLARO', 'MOVISTAR', 'TIGO'];
+	exports.verOperadores = function () { 
+		console.log("________________________"); 
+		console.log("OPERADORES");
+		console.log("------------------------"); 
+		operadores.forEach(operador => console.log(operador));
+		console.log("________________________"); 
+	};
+	
+	const paquetes = new Map([["CLARO", new Map([[1, "$2.000"], [2, "$5.000"], [3, "$10.000"]])],
+														["MOVISTAR", new Map([[1, "$1.000"], [2, "$3.000"], [3, "$10.000"]])],
+														["TIGO", new Map([[1, "$2.000"], [2, "$3.000"], [3, "$5.000"]])]]);
+	exports.verPaquetes = function(operador) {
+		console.log("________________________"); 
+		console.log("PAQUETES " + operador);
+		console.log("------------------------"); 
+		paquetes.get(operador).forEach((paquete, key) => console.log(`Paquete ${key}: ${paquete}`)); 
+		console.log("________________________"); 
+	};
+	
+	exports.recargar =  function(numero, operador, recarga){
+		try {		
+			validarNumero(numero)
+			registroRecargas.set(numero, [operador, paquetes.get(operador).get(recarga), new Date().toLocaleString()]);
+			console.log(`[${registroRecargas.get(numero)[2]}] Se recargo el numero de celular ${numero} del operador ${operador} con el paquete ${recarga} de valor ${registroRecargas.get(numero)[1]}`);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+	
+	exports.verRecargas = function() {
+		console.log("|\tNUMERO\t|\tOPERADOR\t|\tRECARGA\t|\tFECHA\t|");
+		registroRecargas.forEach((detalle, operador) => console.log(`|${operador}\t|\t${detalle[0]}\t|\t${detalle[1]}\t|${detalle[2]}|`))
+	};
+	
+	return exports;
+	
+}());
+
+const recargas =  require('recargas.js');
+recargas.verOperadores();
+
+recargas.verPaquetes("TIGO");
+
+recargas.recargar(3213104614, "MOVISTAR", 2);
+
+recargas.verRecargas();
+
+recargas.recargar("3213104614e", "MOVISTAR", 2);
